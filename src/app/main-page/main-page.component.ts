4447696b10ffed25d07027ff514c791843196e68
Utkarsh7xo <utkarsh7xo@gmail.com>
2023-03-02 19:24:31 +0530

    update

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import events from 'src/event-data.json';
 
 @Component({
   selector: 'app-main-page',
@@ -7,36 +8,36 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent {
-
-  events = [
-    {
-      id: 1111,
-      img: 'jbkbkbkhbk',
-      title: 'Kaladarshan',
-      description: 'This is the description....',
-      date: '2023-03-01',
-      venue: 'New York',
-      favoption: false
-    },
-    {
-      id: 1112,
-      img: 'jbkbkbkhbk',
-      title: 'Kaladarshan',
-      description: 'This is the description....',
-      date: '2023-03-15',
-      venue: 'Los Angeles',
-      favoption: true
-    },
-    {
-      id: 1113,
-      img: 'jbkbkbkhbk',
-      title: 'Kaladarshan',
-      description: 'This is the description....',
-      date: '2023-04-01',
-      venue: 'Chicago',
-      favoption: true
-    }
-  ];
+  events:any = events.events;
+  // events = [
+  //   {
+  //     id: 1111,
+  //     img: 'jbkbkbkhbk',
+  //     title: 'Kaladarshan',
+  //     description: 'This is the description....',
+  //     date: '2023-03-01',
+  //     venue: 'New York',
+  //     favoption: false
+  //   },
+  //   {
+  //     id: 1112,
+  //     img: 'jbkbkbkhbk',
+  //     title: 'Kaladarshan',
+  //     description: 'This is the description....',
+  //     date: '2023-03-15',
+  //     venue: 'Los Angeles',
+  //     favoption: true
+  //   },
+  //   {
+  //     id: 1113,
+  //     img: 'jbkbkbkhbk',
+  //     title: 'Kaladarshan',
+  //     description: 'This is the description....',
+  //     date: '2023-04-01',
+  //     venue: 'Chicago',
+  //     favoption: true
+  //   }
+  // ];
 
   changefav(i: Number) {
     for (let object of this.events) {

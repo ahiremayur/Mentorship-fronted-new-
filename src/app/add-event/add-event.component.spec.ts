439b9e9f85ae25fc29ec55ccf0bb9dd994783fe0
Prakash S Rathod <prakashsrathod2004@gmail.com>
2023-03-02 20:09:47 +0530

    added event

--- src/app/add-event/add-event.component.spec.ts
+++ src/app/add-event/add-event.component.spec.ts
@@ -1,23 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { AddComponent } from './add.component';
 
-import { AddEventComponent } from './add-event.component';
-
-describe('AddEventComponent', () => {
-  let component: AddEventComponent;
-  let fixture: ComponentFixture<AddEventComponent>;
+describe('AddComponent', () => {
+  let component: AddComponent;
+  let fixture: ComponentFixture<AddComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddEventComponent ]
+      declarations: [ AddComponent ],
+      imports: [ FormsModule ]
     })
     .compileComponents();
+  });
 
-    fixture = TestBed.createComponent(AddEventComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the AddComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a new event', () => {
+    const event = {
+      name: 'Test Event',
+      date: '2023-02-28',
+      time: '18:00',
+      venue: 'Test Venue',
+      description: 'Test Description',
+      image: null
+    };
+
+    component.event = event;
+    component.onSubmit();
+
+    expect(component.events).toContain(event);
   });
 });
--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-event.component.css']
 })
 export class AddEventComponent {
+  eventName: string;
+  eventDate: Date;
+  eventTime: string;
+  eventVenue: string;
+  eventDescription: string;
+  eventImage: File;
 
+  onFileSelected(event: Event) {
+    this.eventImage = (event.target as HTMLInputElement).files[0];
+  }
+
+  onSubmit() {
+    // Code to submit the event data to the backend API
+    console.log('Event submitted successfully.');
+  }
 }
